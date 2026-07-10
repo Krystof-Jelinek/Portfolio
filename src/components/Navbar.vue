a209aa4261abef70ef8e590a18d89efe7d1e2a04
Krystof Jelinek <krystof.jelinek@eviden.com>
2026-07-10 14:53:55 +0200

    fixing the layout for phone

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+    import {ref} from 'vue'
     import { useI18n } from 'vue-i18n';
     import LanguageSwitch from './LanguageSwitch.vue';
     const { t } = useI18n()
     
+    const isMenuOpen = ref(false)
+
     const menuItems = [
     {
         key: 'about',
@@ -27,22 +29,37 @@
 
 <template>
   <nav class="navbar">
+
+    <button
+      class="hamburger"
+      @click="isMenuOpen = !isMenuOpen"
+    >
+      ☰
+    </button>
+
     <div class="logo">
       Kryštof Jelínek
     </div>
 
-    <ul class="nav-links">
+    <ul
+      class="nav-links"
+      :class="{ open: isMenuOpen }"
+    >
       <li
         v-for="item in menuItems"
         :key="item.href"
       >
-        <a :href="item.href">
-            {{ t(`navbar.${item.key}`) }}
+        <a
+          :href="item.href"
+          @click="isMenuOpen = false"
+        >
+          {{ t(`navbar.${item.key}`) }}
         </a>
       </li>
     </ul>
-    
-    <LanguageSwitch></LanguageSwitch>
+
+    <LanguageSwitch />
+
   </nav>
 </template>
 
@@ -87,4 +104,66 @@
         width: 36px;
         height: 24px;
     }
+
+    .hamburger {
+        display: none;
+
+        background: none;
+        border: none;
+
+        color: white;
+        font-size: 2rem;
+
+        cursor: pointer;
+
+        margin-right: 1rem;
+    }
+
+    @media (max-width: 768px) {
+
+    .navbar {
+        padding: 1rem;
+        position: relative;
+    }
+
+    .logo {
+        font-size: 1.2rem;
+        margin-right: auto;
+    }
+
+    .hamburger {
+        display: block;
+    }
+
+    .nav-links {
+        display: none;
+
+        position: absolute;
+
+        top: 100%;
+        left: 0;
+
+        width: 100%;
+
+        flex-direction: column;
+
+        background: #1f3035;
+
+        margin: 0;
+        padding: 1rem 0;
+    }
+
+    .nav-links.open {
+        display: flex;
+    }
+
+    .nav-links li {
+        text-align: center;
+    }
+
+    .nav-links a {
+        display: block;
+        padding: 1rem;
+    }
+}
 </style>
